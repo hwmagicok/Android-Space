3283b5c305a98d14b56be607152aecc7da212c8b
hwmagicok <hwmagicok@163.com>
2016-02-28 01:23:46 +0800

    完成列表的显示

---
+++ app/src/main/java/com/hw/weather1/db/WeatherDataDBHelper.java
@@ -0,0 +1,7 @@
+package com.hw.weather1.db;
+
+/**
+ * Created by hw on 2016/2/18.
+ */
+public class WeatherDataDBHelper {
+}
---
+++ app/src/main/java/com/hw/weather1/model/WeatherDataDB.java
@@ -0,0 +1,7 @@
+package com.hw.weather1.model;
+
+/**
+ * Created by hw on 2016/2/19.
+ */
+public class WeatherDataDB {
+}
